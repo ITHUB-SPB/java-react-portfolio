3961f67ac3543b07d9cd733a857436e8aa08e1ff
Lex Trofimov <daslef93@gmail.com>
2026-01-19 18:04:32 +0300

    start implementing main page

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import WorkCard from './components/WorkCard'
+
+function App() {
+  return (
+    <section>
+      <h2>Featured Works</h2>
+      <WorkCard title="A" />
+      <WorkCard title="B" />
+      <WorkCard title="C" />
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/WorkCard/index.tsx
@@ -0,0 +1,22 @@
+import worksDashboardImage from '../../assets/works-dashboards.png'
+import classes from './WorkCard.module.css'
+
+type WorkCardProps = {
+    title: string
+}
+
+export default function WorkCard({ title }: WorkCardProps) {
+    return (
+      <article className={classes.card}>
+        <img className={classes.image} src={worksDashboardImage} alt="work image" />
+        <div>
+          <h3>{title}</h3>
+          <section>
+            <span>2020</span>
+            <span>Dashboard</span>
+          </section>
+          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+        </div>
+      </article>
+    )
+  }
