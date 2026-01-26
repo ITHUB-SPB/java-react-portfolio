f118e8fbfabe9e3e57fdd5e145ff23a38e4e8b1d
Lex Trofimov <daslef93@gmail.com>
2026-01-26 16:52:40 +0300

    wip

--- src/components/WorkCard/index.tsx
+++ src/components/WorkCard/index.tsx
@@ -6,14 +6,14 @@ type WorkCardProps = {
     year: number
 }
 
-export default function WorkCard({ title }: WorkCardProps) {
+export default function WorkCard({ title, year }: WorkCardProps) {
     return (
       <article className={classes.card}>
         <img className={classes.image} src={worksDashboardImage} alt="work image" />
         <div>
           <h3>{title}</h3>
           <section>
-            <span>2020</span>
+            <span>{String(year)}</span>
             <span>Dashboard</span>
           </section>
           <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
